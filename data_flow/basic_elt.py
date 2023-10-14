ff4d8f3ec72efb596b3d7573af19b342bc2991a4
Francis Addae <franco09e@yahoo.com>
2023-10-13 22:31:22 -0400

    Pushing new flow codes

--- data_flow/basic_elt.py
+++ data_flow/basic_elt.py
@@ -1,13 +1,48 @@
-import httpx
-from prefect import flow
+import requests as r
+import pandas as pd
+from prefect import flow, task, get_run_logger
 
+@task
+def extract_from_api():
+    """
+        This function will extract data from the api and transform it into
+        a Pandas DataFrame.
+        note: USING PREFECT 2.0
+        returns: pd.DataFrame or none
+    """
+    logger = get_run_logger()
+    url = 'https://api.coincap.io/v2/assets'
+    header = {
+            "Content-Type": 'application/json',
+            "Accept-Encoding": "deflate"
+            }
 
-@flow
-def get_repo_info():
-    url = "https://api.github.com/repos/PrefectHQ/prefect"
-    response = httpx.get(url)
-    response.raise_for_status()
-    repo = response.json()
-    print("PrefectHQ/prefect repository statistics 🤓:")
-    print(f"Stars 🌠 : {repo['stargazers_count']}")
-    print(f"Forks 🍴 : {repo['forks_count']}")
+    response_data = r.get(url, headers=header)
+
+    print(f'This is the response status code: {response_data.status_code}')
+
+    # Store JSON data in API_Data
+    API_Data = response_data.json()
+
+    # Print json data using loop
+    logger.info(API_Data.keys())
+
+    for main_key in API_Data.keys():
+        if main_key == 'data':
+
+            # the data in list of embeded dictionaries
+            df = pd.DataFrame.from_dict(API_Data['data'])
+    df['timestamp'] = API_Data['timestamp']
+    df = df.convert_dtypes()
+    logger.info(df)
+    logger.info(df.info())
+    logger.info(df.dtypes)
+
+    return df if True else None
+
+@flow(retries=3, retry_delay_seconds=5)
+def main_flow_run():
+    extract_from_api()
+
+if __name__ == "__main__":
+    main_flow_run()
