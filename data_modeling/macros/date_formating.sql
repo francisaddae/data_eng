9a1cbfa51b891bd5af84446772fd39fd8eb3386c
Francis Addae <franco09e@yahoo.com>
2023-11-08 21:26:03 -0500

    Adding new macro for date functions

--- data_modeling/macros/date_formating.sql
+++ data_modeling/macros/date_formating.sql
@@ -8,3 +8,8 @@
     -- Cast a date into a string format, based on the notation given
     TO_CHAR({{ col }}::DATE, '{{ format_method }}' )
 {% endmacro %}
+
+{% macro at_date_with_format_op_2(col, format_method) %}
+    -- Another format used to format a date into a paritcular format
+    to_date( {{ col }}::DATE, '{{ format_method }}')
+{% endmacro %}
