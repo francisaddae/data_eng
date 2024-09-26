1e3bc5d667b43d4957c391587eacb0f8a7f39e5f
Francis Addae <franco09e@yahoo.com>
2024-09-25 23:57:54 -0400

    removing absolute path

--- data_flow/flows.py
+++ data_flow/flows.py
@@ -1,7 +1,7 @@
-from data_flow.covidELT.extract import covidDataELT
-from data_flow.lemonadeELT.extract import lemonadeFLOW
+# from covidELT.extract import covidDataELT
+from lemonadeELT.extract import lemonadeFLOW
 
 
 if __name__ == "__main__":
     lemonadeFLOW()
-    covidDataELT()
+    # covidDataELT()
